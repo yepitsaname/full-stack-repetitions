4f8dd56ceecacb7a47dd7bc6f563f985f52c7141
yepitsaname <fernando.g.berry@gmail.com>
2025-08-29 12:18:52 -0400

    finishes full stack. starting scorched earth 3

---
+++ app/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+export default function App (){
+  const [movies, setMovies] = useState([]);
+
+  useEffect(()=>{
+    fetch('http://localhost:8080/movies')
+    .then( res => {
+      if( res.status != 200 ){ throw new Error(res.statusText) };
+      return res.json();
+    })
+    .then( data => setMovies(data) )
+    .catch(err => console.error(err));
+  },[])
+  return (
+    <>
+    {movies.map((e,i) => <p key={i}>{e.title} {e.main_character} {e.year_released}</p>)}
+    </>
+  )
+}
