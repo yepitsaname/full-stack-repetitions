dd6f13b69fc4e0ef597b5ad30dc527f60d3b7193
yepitsaname <fernando.g.berry@gmail.com>
2025-08-29 10:57:49 -0400

    creates the tables and data! Time for scorched earth 1

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: `postgresql://postgres:docker@localhost:5432/movies`
+  }
+};
---
+++ migrations/20250829144953_favorites.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('favorites', table => {
+    table.increments('fav_id');
+    table.string('title', 256);
+    table.string('main_character', 256);
+    table.smallint('year_released');
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('favorites');
+};
